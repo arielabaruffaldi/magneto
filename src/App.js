3361b4860a6dd9b31b185a3c7151ad3bcdd22074
arielabaruffaldi <baruffaldiariela@gmail.com>
2021-09-21 23:20:31 -0300

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.scss';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+
 import Nav from './components/Nav/Nav';
 import Main from './components/Main/Main';
-import Detector from './pages/Detector/Detector';
-import { useMediaQuery } from "react-responsive";
+
 import NavMobile from "./components/NavMobile/NavMobile";
 import { menu } from './utils/menu';
+
+import Detector from './pages/Detector/Detector';
+import Cargar from './pages/Cargar/Cargar';
+
+import './App.scss';
+import Mutantes from "./pages/Mutantes/Mutantes";
 
 function App() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -16,6 +22,12 @@ function App() {
         <Switch>
           <Route exact path="/detector">
             <Detector />
+          </Route>
+          <Route exact path="/cargar">
+            <Cargar />
+          </Route>
+          <Route exact path="/mutantes">
+            <Mutantes />
           </Route>
         </Switch>
       </Main>
